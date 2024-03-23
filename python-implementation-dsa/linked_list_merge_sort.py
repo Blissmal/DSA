472cdbd17cff52708c3367bb5ce5a1e6ec28c927
Blissmal <121401934+Blissmal@users.noreply.github.com>
2024-03-23 23:48:41 +0300

    added linked list merge sort

---
+++ python-implementation-dsa/linked_list_merge_sort.py
@@ -0,0 +1,176 @@
+class Node:
+    """
+    An object for storing a single node of a linked list,
+    Models two attributes - data and the link to the next node in the list
+    """
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+
+class LinkedList:
+    """
+    Singly linked list
+    """
+
+    def __init__(self):
+        self.head = None
+
+    def size(self):
+        """
+        Returns the number of nodes in the list
+        Takes O(n) time
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
+
+    def add(self, data):
+        """
+        Adds new node containing data at the head of a list
+        This operation takes constant time
+        """
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def node_at_index(self, index):
+        if index == 0:
+            return self.head
+        else:
+            current = self.head
+            position = 0
+
+            while position < index:
+                current = current.next_node
+                position += 1
+
+            return current
+        
+    def __repr__(self):
+        """
+        Returns a string representation of the list
+        Takes O(n) time
+        """
+
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+
+            current = current.next_node
+        return '-> '.join(nodes)
+
+
+
+def merge_sort(linked_list):
+    """
+    Sort a linked list in ascending order
+    - Recursively divide the linked list into sublists containing a single node
+    - Repeatedly merge the sublists to produce sorted sublists until one remains
+
+    Returns a sorted linked list
+    """
+    if linked_list.size() == 1:
+        return linked_list
+    elif linked_list.head is None:
+        return linked_list
+
+    left_half, right_half = split(linked_list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
+
+
+def split(linked_list):
+    """
+    Divide the unsorted list at midpoints into sublists
+
+    """
+    if linked_list is None or linked_list.head is None:
+        left_half = LinkedList()
+        right_half = None
+
+        return left_half, right_half
+    else:
+        size = linked_list.size()
+        mid = size // 2
+
+        mid_node = linked_list.node_at_index(mid - 1)
+
+        left_half = linked_list
+        right_half = LinkedList()
+        right_half.head = mid_node.next_node
+        mid_node.next_node = None
+
+        return left_half, right_half
+
+
+def merge(left, right):
+    """
+    Merges two lists, sorting by data in nodes
+    Returns a new merged list
+    """
+
+    merged = LinkedList()
+
+    current = merged.head = Node(0)
+
+    left_head = left.head
+    right_head = right.head
+
+    while left_head or right_head:
+        if left_head is None:
+            current.next_node = right_head
+            right_head = right_head.next_node
+
+        elif right_head is None:
+            current.next_node = left_head
+            left_head = left_head.next_node
+
+        else:
+            left_data = left_head.data
+            right_data = right_head.data
+
+            if left_data < right_data:
+                current.next_node = left_head
+                left_head = left_head.next_node
+            else:
+                current.next_node = right_head
+                right_head = right_head.next_node
+
+        current = current.next_node
+
+    head = merged.head.next_node
+    merged.head = head
+
+    return merged
+
+
+list = LinkedList()
+list.add(10)
+list.add(2)
+list.add(44)
+list.add(15)
+list.add(200)
+
+print("Original list:", list)
+sorted_linked_list = merge_sort(list)
+print("Sorted list:", sorted_linked_list)
